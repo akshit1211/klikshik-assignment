f0b7ae94eea4de86ab3677746d527ab018020712
akshit1211 <83713593+akshit1211@users.noreply.github.com>
2024-10-13 22:12:42 +0530

    Update UploadedImages.js

--- image-uploader/src/components/UploadedImages.js
+++ image-uploader/src/components/UploadedImages.js
@@ -1,33 +1,16 @@
-// import React from 'react';
-// import { Grid } from '@mui/material';
-
-// const UploadedImages = ({ images }) => {
-//     return (
-//         <Grid container spacing={2}>
-//             {images.map((image, index) => (
-//                 <Grid item key={index}>
-//                     <img src={image} alt={`Uploaded ${index}`} width="100" />
-//                 </Grid>
-//             ))}
-//         </Grid>
-//     );
-// };
-
-// export default UploadedImages;
-
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Box } from '@mui/material';
 
 const UploadedImages = ({ images }) => {
     return (
-        <Grid container spacing={2}>
+        <Box
+            display="grid"
+            gridTemplateColumns="repeat(auto-fill, minmax(100px, 1fr))"
+            gap={16}
+        >
             {images.map((image, index) => (
-                <Grid item key={index}>
-                    <img src={image} alt={`Uploaded ${index}`} width="100" />
-                </Grid>
+                <Box key={index} component="img" src={image} alt={`Uploaded ${index}`} width="100" />
             ))}
-        </Grid>
+        </Box>
     );
 };
-
-export default UploadedImages;
